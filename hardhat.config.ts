5cce30c86aa16374d5849760bada119057e83733
Felipe Simões <ffelipesimoes@gmail.com>
2024-11-02 23:44:19 +0000

    feat: initial setup

---
+++ hardhat.config.ts
@@ -0,0 +1,53 @@
+import { HardhatUserConfig } from "hardhat/types";
+import "@nomiclabs/hardhat-ethers";
+import "@nomicfoundation/hardhat-verify";
+import "@nomicfoundation/hardhat-ignition";
+
+
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PRIVATE_KEY: string = process.env.PRIVATE_KEY as string;
+
+const config: HardhatUserConfig = {
+  networks: {
+    amoy: {
+      url: "https://polygon-amoy.drpc.org",
+      accounts: [PRIVATE_KEY],
+    },
+    holesky: {
+      url: "https://ethereum-holesky-rpc.publicnode.com",
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: process.env.ETHERSCAN_API!,
+      holesky: process.env.ETHERSCAN_API!,
+      polygonAmoy: process.env.POLYGONSCAN_API!,
+    },
+    customChains: [
+      {
+        network: "holesky",
+        chainId: 17000,
+        urls: {
+          apiURL: "https://api-holesky.etherscan.io/api",
+          browserURL: "https://holesky.etherscan.io/",
+        },
+      },
+    ],
+  },
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
+
+export default config;
